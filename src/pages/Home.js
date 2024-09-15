ad1b8fa57b9a8759c412eb9884ddf5b8a6988d83
Koushik2208 <gorrekoushik@gmail.com>
2024-09-15 16:50:26 +0530

    redesigned the entire website

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,95 +1,106 @@
+import { useState } from "react";
 const Home = () => {
+    const images = [
+        require("./images/banner-1.webp"),
+        require("./images/banner-2.webp"),
+        require("./images/banner-3.webp"),
+        require("./images/banner-4.webp")
+    ]
+
+    const [serviceImg, setServiceImg] = useState(null);
     return (
         <div>
-            <div className="hero-img"></div>
+            <section className="hero-img">
+                <div className="hero">
+                    <div className="container">
+                        <h1>Building Dreams, Crafting Legacies.</h1>
+                        <p>Creating spaces where innovation meets craftsmanship, and every structure tells a story.</p>
+                        <button>Contact us</button>
+                    </div>
+                </div>
+            </section>
             <section id="about-us">
                 <div className="container">
-                    <div className="row">
-                        <div className="col-md-6">
-                            <img src={require("./images/amanda-marie-LCJ9iOli-uE-unsplash_11zon.webp")} alt="our philosophy" className="img-fluid" />
-                        </div>
-                        <div className="col-md-6">
-                            <h3>Our Philosophy</h3>
-                            <p>
-                                Our philosophy centers on the belief that a home is more than just a
-                                structure. It's a sanctuary that embodies comfort, beauty and functionality.
-                                We are dedicated to creating spaces that meet and exceed our client's
-                                expectations through innovative and effective design. Our commitment to
-                                excellence ensures that every project is built with the highest quality
-                                materials and craftsmanship, fostering environments where people can
-                                thrive. We strive to transform architectural dreams into reality, one
-                                meticulously designed home at a time.
-                            </p>
-                        </div>
+                    <h3>Our Philosophy</h3>
+                    <div className="philosophy">
+                        <img src={require("./images/our-philosophy.webp")} alt="our philosophy" />
+                        <p>
+                            Our philosophy centers on the belief that a home is more than just a
+                            structure. It's a sanctuary that embodies comfort, beauty and functionality.
+                            We are dedicated to creating spaces that meet and exceed our client's
+                            expectations through innovative and effective design. Our commitment to
+                            excellence ensures that every project is built with the highest quality
+                            materials and craftsmanship, fostering environments where people can
+                            thrive. We strive to transform architectural dreams into reality, one
+                            meticulously designed home at a time.
+                        </p>
                     </div>
+                </div>
+            </section>
+            <section id="training">
+                <div className="container">
+                    <h3>Training Courses</h3>
+                    <ul>
+                        <li>AUTOCAD</li>
+                        <li>REVIT</li>
+                        <li>ETABS</li>
+                        <li>STAAD PRO</li>
+                        <li>SKETCH UP</li>
+                    </ul>
                 </div>
             </section>
             <section id="services">
                 <div className="container">
-                    <div className="row">
-                        <div className="col-md-6">
-                            <h3>OUR SERVICES</h3>
-                            <ul>
-                                <li>Architectural Design</li>
-                                <li>Structural (RCC & PEB) Designs</li>
-                                <li>MEP services</li>
-                                <li>Landscape Architecture</li>
-                                <li>Interior Design</li>
-                                <li>Valuation, Charted Engineers</li>
-                                <li>Construction</li>
-                            </ul>
+                    <h3>Our Services</h3>
+                    <div className="cards">
+                        <div className="service-card">
+                            <div className="overlay"></div>
+                            <div className="holder">
+                                <img src={require('./images/blueprint.webp')} alt="architectural" />
+                            </div>
+                            <div className="title">Architectural Design</div>
                         </div>
-                        <div className="col-md-6">
-                            <h3>TRAINING COURSES</h3>
-                            <ul>
-                                <li>AUTOCAD</li>
-                                <li>REVIT</li>
-                                <li>ETABS</li>
-                                <li>STAAD PRO</li>
-                                <li>SKETCH UP</li>
-                            </ul>
+                        <div className="service-card">
+                            <div className="overlay"></div>
+                            <div className="holder">
+                                <img src={require('./images/beam.webp')} alt="structural" />
+                            </div>
+                            <div className="title">Structural (RCC & PEB) Designs</div>
                         </div>
-                    </div>
-                </div>
-            </section>
-            <section>
-                <div className="container">
-                    <div className="row d-flex align-items-center">
-                        <div className="col-md-6">
-                            <p>Every brick, every
-                                pillar, every wall has
-                                a part in the story.
-                                They contribute to
-                                the narrative of the
-                                entire structure.</p>
+                        <div className="service-card">
+                            <div className="overlay"></div>
+                            <div className="holder">
+                                <img src={require('./images/wrench.webp')} alt="mep" />
+                            </div>
+                            <div className="title">MEP services</div>
                         </div>
-                        <div className="col-md-6">
-                            <img src={require('./images/abbe-sublett-nxZDMUQhN4o-unsplash_11zon.webp')} alt="site" className="img-fluid" />
+                        <div className="service-card">
+                            <div className="overlay"></div>
+                            <div className="holder">
+                                <img src={require('./images/park.webp')} alt="landscape" />
+                            </div>
+                            <div className="title">Landscape Architecture</div>
                         </div>
-                    </div>
-                </div>
-            </section>
-            <section id="projects">
-                <div className="container">
-                    <h4>Selected Projects</h4>
-                    <div className="row d-flex align-items-center">
-                        <div className="col-md-6">
-                            <img src={require('./images/bbf9e62768e5d67b73141954e96764a9eef3f035.webp')} alt="selected project" className="img-fluid" />
+                        <div className="service-card">
+                            <div className="overlay"></div>
+                            <div className="holder">
+                                <img src={require('./images/furbishing.webp')} alt="interior" />
+                            </div>
+                            <div className="title">Interior Design</div>
                         </div>
-                        <div className="col-md-6">
-                            <img src={require('./images/building-construction-work-services.webp')} alt="selected project" className="img-fluid" />
+                        <div className="service-card">
+                            <div className="overlay"></div>
+                            <div className="holder">
+                                <img src={require('./images/documentation.webp')} alt="valuation" />
+                            </div>
+                            <div className="title">Valuation, Charted Engineers</div>
                         </div>
-                    </div>
-                </div>
-            </section>
-            <section>
-                <div className="container">
-                    <div className="row d-flex align-items-center">
-                        <div className="col-md-6">
-                            Layout demonstration
-                        </div>
-                        <div className="col-md-6">
-                            <img src={require('./images/layout.webp')} alt="layout" className="img-fluid" />
+                        <div className="service-card">
+                            <div className="overlay"></div>
+                            <div className="holder">
+                                <img src={require('./images/crane.webp')} alt="construction" />
+                            </div>
+                            <div className="title">Construction</div>
                         </div>
                     </div>
                 </div>
